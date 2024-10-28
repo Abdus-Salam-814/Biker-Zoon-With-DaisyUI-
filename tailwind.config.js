21575dde861556afef8a3830de8e92bcb4df7934
slam814 <abdussalam6111997@gmail.com>
2024-10-28 07:00:16 +0600

    i added primary resourse

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+
+  module.exports = {
+    theme: {
+      fontFamily: {
+        sans: [
+          '"Inter var", sans-serif',
+          {
+            fontFeatureSettings: '"cv11", "ss01"',
+            fontVariationSettings: '"opsz" 32'
+          },
+        ],
+      },
+    },
+  }
